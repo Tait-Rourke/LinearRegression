142a47c83c9f19e660229a75b704cf4c79dcb2a9
Tait-Rourke <Taitrourke23@gmail.com>
2023-11-26 16:50:10 -0500

    Salary and Experience Model

---
+++ linearReg.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv("/Users/taitrourke/Desktop/Sklearn Test/salary.csv")
+pd.set_option('display.max_columns', None)
+
+# Check data for null values and display null count per column
+print("Null Values in Dataset")
+print(data.isnull().sum())
+
+# Sort the data by years, this allows for easier analysis later on
+data.sort_values(["years"], axis=0,ascending=[True], inplace=True)
+
+# Show the data being utilized
+print(data)
+
+X = data[['years']]
+y = data['salary']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+
+# Initialize and train the linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f'Mean Squared Error: {mse:.2f}')
+print(f'R-squared (R2) Score: {r2:.2f}')
+
+# Plot the original data and the regression line
+plt.scatter(X, y, label='Original Data')
+plt.plot(X_test, y_pred, color='red', linewidth=2, label='Regression Line')
+plt.xlabel('X')
+plt.ylabel('y')
+plt.legend()
+plt.show()
